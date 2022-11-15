a7f1cf41a9d4d7260a72ee51fd454cd07862b046
YOUNG <moxnox63@gmail.com>
2022-11-15 11:51:06 +0900

    user update

--- src/main/java/com/younghyeon/diary/model/service/UserService.java
+++ src/main/java/com/younghyeon/diary/model/service/UserService.java
@@ -1,4 +1,17 @@
 package com.younghyeon.diary.model.service;
 
+import com.younghyeon.diary.model.dto.User;
+
 public interface UserService {
+    // 사용자 등록
+    int insertUser(User user);
+
+    // 사용자 수정
+    void updateUser(User user);
+
+    // 사용자 삭제
+    int deleteUser(User user);
+
+    // 사용자 검색(id & pw)
+    User searchUser(User user);
 }
--- src/main/java/com/younghyeon/diary/model/service/UserServiceImpl.java
+++ src/main/java/com/younghyeon/diary/model/service/UserServiceImpl.java
@@ -1,4 +1,33 @@
 package com.younghyeon.diary.model.service;
 
-public class UserServiceImpl {
+import com.younghyeon.diary.model.dao.UserDao;
+import com.younghyeon.diary.model.dto.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public int insertUser(User user) {
+        return 0;
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public int deleteUser(User user) {
+        return 0;
+    }
+
+    @Override
+    public User searchUser(User user) {
+        return null;
+    }
 }
