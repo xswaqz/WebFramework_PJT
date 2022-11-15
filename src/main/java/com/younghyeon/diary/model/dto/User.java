4897e7476873b05d7ea427415b2a4278732b1eb8
YOUNG <moxnox63@gmail.com>
2022-11-15 11:25:53 +0900

    new dto and user.dto written

---
+++ src/main/java/com/younghyeon/diary/model/dto/User.java
@@ -0,0 +1,82 @@
+package com.younghyeon.diary.model.dto;
+
+public class User {
+    private String id;
+    private String password;
+    private String name;
+    private int age;
+    private String email;
+    private String address;
+
+    public User() {
+    }
+
+    public User(String id, String password, String name, int age, String email, String address) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/younghyeon/diary/model/service/VideoServiceImpl.java
@@ -0,0 +1,4 @@
+package com.younghyeon.diary.model.service;
+
+public class VideoServiceImpl {
+}
