f1cddedd220b62e3ac09a20a9c75cf9261d75485
hiflo <xswaqz@naver.com>
2022-11-15 09:47:36 +0900

    create project

---
+++ src/main/java/com/younghyeon/diary/WebFrameworkPjtApplication.java
@@ -0,0 +1,13 @@
+package com.younghyeon.diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFrameworkPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFrameworkPjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/younghyeon/diary/WebFrameworkPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younghyeon.diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFrameworkPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
