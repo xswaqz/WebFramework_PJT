3bba1d733c50e894f7b3dba2d20c001ed53dc697
hiflo <xswaqz@naver.com>
2022-11-15 16:31:29 +0900

    videorestcontroller

--- src/main/java/com/younghyeon/diary/controller/VideoRestController.java
+++ src/main/java/com/younghyeon/diary/controller/VideoRestController.java
@@ -1,4 +1,59 @@
 package com.younghyeon.diary.controller;
 
+import com.younghyeon.diary.model.dto.Video;
+import com.younghyeon.diary.model.service.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class VideoRestController {
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+
+    @Autowired
+    private VideoService videoService;
+
+    @GetMapping("/video")
+    public ResponseEntity<List<Video>> list(String mode, String keyword){
+        HashMap<String, String> params = new HashMap<String, String>();
+
+        params.put("mode", mode);
+        params.put("keyword", keyword);
+
+        return new ResponseEntity<List<Video>>(videoService.getVideoList(params), HttpStatus.OK);
+    }
+
+    @PostMapping("/video")
+    public ResponseEntity<String> write(Video video){
+        videoService.writeVideo(video);
+        return new ResponseEntity<String>(SUCCESS, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/video/{id}")
+    public ResponseEntity<Video> detail(@PathVariable int id){
+        return new ResponseEntity<Video>(videoService.getVideo(id), HttpStatus.OK);
+    }
+
+    //게시글 수정
+    @PutMapping("/video")
+    public ResponseEntity<String> update(Video video){
+        videoService.modifyVideo(video);
+        return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+    }
+
+    //게시글 삭제
+    @DeleteMapping("/video/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id){
+        if(videoService.removeVideo(id)){
+            return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+        }
+        return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+    }
+    
 }
