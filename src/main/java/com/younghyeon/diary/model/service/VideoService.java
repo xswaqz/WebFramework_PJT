8bbdfa733c1726343be87fa36a34f76c36e82c7e
YOUNG <moxnox63@gmail.com>
2022-11-15 16:45:33 +0900

    video complete

--- src/main/java/com/younghyeon/diary/model/service/VideoService.java
+++ src/main/java/com/younghyeon/diary/model/service/VideoService.java
@@ -1,4 +1,24 @@
 package com.younghyeon.diary.model.service;
 
+
+import com.younghyeon.diary.model.dto.Video;
+
+import java.util.HashMap;
+import java.util.List;
+
 public interface VideoService {
+    // 비디오 목록
+    List<Video> videoList(HashMap<String, String> params);
+
+    // 비디오 등록
+    void registVideo(Video video);
+
+    // 사용자 수정
+    void modifyVideo(Video video);
+
+    // 사용자 상세
+    Video videoDetail(int id);
+
+    // 사용자 삭제
+    boolean removeVideo(int id);
 }
