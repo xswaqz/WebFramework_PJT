198be8b2124dfd61d48d986c8db2e26cade6549b
YOUNG <moxnox63@gmail.com>
2022-11-15 10:34:22 +0900

    config and setting update

--- src/main/java/com/younghyeon/diary/config/DBConfig.java
+++ src/main/java/com/younghyeon/diary/config/DBConfig.java
@@ -1,7 +1,6 @@
 package com.younghyeon.diary.config;
 
 import org.mybatis.spring.annotation.MapperScan;
-
 @MapperScan(basePackages = "com.younghyeon.diary.model.dao")
 public class DBConfig {
 
--- src/main/java/com/younghyeon/diary/config/WebConfig.java
+++ src/main/java/com/younghyeon/diary/config/WebConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/swagger-ui/**")
