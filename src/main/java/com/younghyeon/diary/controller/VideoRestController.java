fdc7681e0d5f6e6439e80d4b72d86289e4f2fe36
YOUNG <moxnox63@gmail.com>
2022-11-15 16:52:27 +0900

    video controller change

--- src/main/java/com/younghyeon/diary/controller/VideoRestController.java
+++ src/main/java/com/younghyeon/diary/controller/VideoRestController.java
@@ -1,9 +1,7 @@
 package com.younghyeon.diary.controller;
 
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-=======
 import com.younghyeon.diary.model.dto.Video;
 import com.younghyeon.diary.model.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,49 +11,47 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
->>>>>>> 07f6197066437ebc5570f152f1c28fe94395ef01
 
 @RestController
 @RequestMapping("/api")
 public class VideoRestController {
-<<<<<<< HEAD
 
-=======
     private static final String SUCCESS = "success";
     private static final String FAIL = "fail";
 
     @Autowired
     private VideoService videoService;
 
+    // 비디오 검색
     @GetMapping("/video")
     public ResponseEntity<List<Video>> list(String mode, String keyword){
         HashMap<String, String> params = new HashMap<String, String>();
-
         params.put("mode", mode);
         params.put("keyword", keyword);
-
-        return new ResponseEntity<List<Video>>(videoService.getVideoList(params), HttpStatus.OK);
+        return new ResponseEntity<List<Video>>(videoService.videoList(params), HttpStatus.OK);
     }
 
+    // 비디오 등록
     @PostMapping("/video")
     public ResponseEntity<String> write(Video video){
-        videoService.writeVideo(video);
+        videoService.registVideo(video);
         return new ResponseEntity<String>(SUCCESS, HttpStatus.CREATED);
     }
 
+    // 비디오 상세
     @GetMapping("/video/{id}")
     public ResponseEntity<Video> detail(@PathVariable int id){
-        return new ResponseEntity<Video>(videoService.getVideo(id), HttpStatus.OK);
+        return new ResponseEntity<Video>(videoService.videoDetail(id), HttpStatus.OK);
     }
 
-    //게시글 수정
+    // 비디오 수정
     @PutMapping("/video")
     public ResponseEntity<String> update(Video video){
         videoService.modifyVideo(video);
         return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
     }
 
-    //게시글 삭제
+    // 비디오 삭제
     @DeleteMapping("/video/{id}")
     public ResponseEntity<String> delete(@PathVariable int id){
         if(videoService.removeVideo(id)){
@@ -64,5 +60,4 @@ public class VideoRestController {
         return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
     }
     
->>>>>>> 07f6197066437ebc5570f152f1c28fe94395ef01
 }
